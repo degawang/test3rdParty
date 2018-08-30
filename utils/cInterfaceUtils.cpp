0af6ab65e91a0f748504be6368e6b87fabd2ead1
degawong <degawong@hotmail.com>
2018-08-30 19:32:22 +0800

    - add input library

--- utils/cInterfaceUtils.cpp
+++ utils/cInterfaceUtils.cpp
@@ -1,3 +1,6 @@
+
+#include <cv.h>
+#include <amcomdef.h>
 #include "cInterfaceUtils.h"
 
 #include<iostream>
@@ -7,6 +10,7 @@ void degawong::cInterfaceUtils::doAlgorithm() {
 	/* to do what u have to do */
 	
 	
+	std::cout << mcvFastSqrts64(85) << std::endl;
 	std::cout << "to do what u have to do" << std::endl;
 
 
